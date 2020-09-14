d95d510c12690261ed2bc67b63ca8183fb098105
EricNgo1972 <ngo-thanh.tung@spc-technology.com>
2020-09-14 05:14:44 -0400

    Bug Report

---
+++ TestPortal/TestPortal/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SPC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestPortal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                var login = new ClaimsPrincipal(new ClaimsIdentity());
+                Csla.ApplicationContext.User = login;
+
+                var list = await PersonInfoList.GetInfoListAsync(null);
+
+                var list2 =  PersonInfoList.GetInfoList(null);
+
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+        }
+    }
+}
---
+++ TestPortal/TestPortal/ServiceConfig.cs
@@ -0,0 +1,33 @@
+﻿using Csla.Configuration;
+using System;
+
+namespace SPC.UI
+{
+    static class ServiceConfig
+    {
+        //const string AppHostUrl = "http://localhost:5000/api/DataPortal";
+
+        //const string AppHostUrl = "http://192.168.1.71:8002/DataPortal";
+
+        //const string AppHostUrl = "http://192.168.1.71/HostingWebCore/DataPortal";
+
+        const string AppHostUrl = "http://192.168.1.71:8000/DataPortal";
+
+        public static void Init()
+        {
+
+            Console.WriteLine($"Set Dataportal Proxy : {AppHostUrl}");
+
+            new Csla.Configuration.CslaConfiguration().DataPortal().DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), AppHostUrl);
+
+
+            //Csla.ApplicationContext.DataPortalProxy = typeof(Csla.DataPortalClient.HttpProxy).AssemblyQualifiedName;
+
+            //Csla.ApplicationContext.DataPortalUrlString = AppHostUrl;
+
+
+        }
+
+
+    }
+}
